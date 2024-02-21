cdc6f36bd7abe12cd555f9d6ac85da1ce8b20db1
Dusan Krtinic <dule147@gmail.com>
2024-02-22 00:37:17 +0100

    Komentari2

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-
+// selektujem kontejner
 let container = document.querySelector('.container');
 
 createGrid();
-
+// tu sam selektovao sve kvadrate
 let allBoxes = document.querySelectorAll('.box');
 let message = document.querySelector('.message');
 
@@ -21,17 +21,17 @@ let allCombinations = [
 let sign = "O";
 let playerTurn =true;
 let winner=false;
-
+// pravim devet divova
 function createGrid(){
 
     for(let i=0;i<9;i++){
-
+// napravim DIV
         let box = document.createElement('DIV');
-        
+        // ovde mu dodam klasu
         box.className = "box";
-
+// dodam mu da mogu da kliknem
         box.addEventListener('click',insertSign);
-
+// i stavim ga u kontejner
         container.appendChild(box);
 
     }
@@ -53,13 +53,13 @@ function getEmptyBoxes() {
     return emptyBoxes; 
 }
 
-
+// kad kliknem uzmem bas taj koji sam kliknuo i stavim mu X this mi omogucava da stavim bas taj na koji sam kliknuo X
 function insertSign(){
 
     if (this.innerHTML === '' && playerTurn) {
 
         this.innerHTML = "X";
-
+// kada kliknem i stavim znak da skine sa njega opciju da mogu opet na to mesto da kliknem
         this.removeEventListener('click', insertSign);
         
         playerTurn = false;
@@ -98,7 +98,7 @@ function computerTurn(){
 }
 
 
-
+// ovde se poziva funkcija kojom  izlazim iz funkcije insertSign
 function checkLines(){
 
 
@@ -107,7 +107,7 @@ function checkLines(){
             let box1 = allBoxes[oneCombination[0]];
             let box2 = allBoxes[oneCombination[1]];
             let box3 = allBoxes[oneCombination[2]];
-
+// X je string tip podatka i zato na kraju stoji znak uzvika da proveri da ne moze da tri prazne kocke da racuna da je pogodio kao pobednik
             if(box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML && box1.innerHTML !=""){
                 box1.style.background = "tomato";
                 box2.style.background = "tomato";
@@ -123,7 +123,7 @@ function checkLines(){
         });
 }
 
-
+// kada pogodim tri iste da posle svakoj kutiji skinem mogucnost da je kliknem
 function stopGame(){
     allBoxes.forEach(box=>box.removeEventListener('click',insertSign));
 }
